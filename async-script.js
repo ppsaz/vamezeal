0d29fdceb1a9acb06c9cf7465ce069ae965a5d3f
Nafiu Jibril <121317397+ppsaz@users.noreply.github.com>
2024-02-17 13:22:36 +0100

    Add files via upload

---
+++ async-script.js
@@ -0,0 +1,23 @@
+// async-script.js
+
+// Load an external script asynchronously using the "defer" attribute
+// This script won't block the page rendering
+// It will execute after the DOM is fully built
+// You can replace the URL with the actual path to your script
+loadScript("/path/to/your/external-script.js");
+
+// Example function to load a script
+function loadScript(src) {
+  let script = document.createElement('script');
+  script.src = src;
+  script.async = true; // Set to "true" for asynchronous loading
+  document.body.append(script);
+}
+
+// Other code or functions specific to your project can follow here
+// ...
+// Adsense Ad blocking recovery
+<script async src="https://fundingchoicesmessages.google.com/i/pub-9324524444765938?ers=1"
+  nonce="1i5Za73HUzAgT2OV9vjKgQ"></script>
+<script
+  nonce="1i5Za73HUzAgT2OV9vjKgQ">(function () { function signalGooglefcPresent() { if (!window.frames['googlefcPresent']) { if (document.body) { const iframe = document.createElement('iframe'); iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;'; iframe.style.display = 'none'; iframe.name = 'googlefcPresent'; document.body.appendChild(iframe); } else { setTimeout(signalGooglefcPresent, 0); } } } signalGooglefcPresent(); })();</script>
